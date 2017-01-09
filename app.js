404f11c0de5de8ae352dbe5e2965b1282f3449a4
html5col <html5master@sina.com>
2017-01-09 13:28:36 +0800

    revert

--- app.js
+++ app.js
@@ -11,8 +11,13 @@ require('./src/libs/mongoose-connect');
 
 //middlewares
 //Note: bodyParser() takes care of turning your request data into a JavaScript object on the server automatically.
-server.use(restify.bodyParser({ mapParams: true }));
-server.use(restify.queryParser({ mapParams: true }));
+server.use(restify.bodyParser());
+
+//Parses the HTTP query string (i.e., /foo?id=bar&name=mark). If you use this, the parsed content will always be available in req.query, additionally params are merged into req.params.
+server.use(restify.queryParser());
+//Parses out the Accept header, and ensures that the server can respond to what the client asked for. You almost always want to just pass in server.acceptable here, as that's an array of content types the server knows how to respond to (with the formatters you've registered). If the request is for a non-handled type, this plugin will return an error of 406.
+server.use(restify.acceptParser(server.acceptable));
+
 
 server.get('/', function(req,res,next){
   console.log('enteringinto frontpage');
